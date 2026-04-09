f666409ad78760a6f17400e38a1afc787722a861
24karan04 <karanpreets240@gmail.com>
2026-04-09 14:56:45 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-LOG_FILE = "data/logs.txt"
-
-def log_event(event):
-    time = datetime.datetime.now()
-    with open(LOG_FILE, "a") as f:
-        f.write(f"{time} : {event}\n")
